bb291b51866f390639fec038187a9cb422eca94e
Eric Lombrozo <elombrozo@gmail.com>
2013-10-08 03:15:12 -0700

    WIP

--- tests/hdwallets.cpp
+++ tests/hdwallets.cpp
@@ -6,13 +6,20 @@
 using namespace Coin;
 using namespace std;
 
-void show(const HDKeychain& keychain, bool showfields = false)
+const uchar_vector SEED("000102030405060708090a0b0c0d0e0f");
+const uint32_t CHAIN[] = { 0x80000000, 0x00000001, 0x80000002, 0x00000002, 0x3b9aca00 };
+const unsigned int CHAIN_LENGTH = sizeof(CHAIN)/sizeof(uint32_t);
+
+void showKey(const HDKeychain& keychain)
 {
-    if (showfields) {
-        std::cout << keychain.toString();
-    }
-    std::cout << "  * ext " << (keychain.isPrivate() ? "prv" : "pub") << ": " << toBase58Check(keychain.extkey()) << std::endl;
-//    std::cout << "extkey: " << uchar_vector(keychain.extkey()).getHex() << std::endl;
+    cout << "  * ext " << (keychain.isPrivate() ? "prv" : "pub") << ": " << toBase58Check(keychain.extkey()) << endl;
+}
+
+void showStep(const string& chainname, const HDKeychain& pub, const HDKeychain& prv)
+{
+    cout << "* [" << chainname << "]" << endl;
+    showKey(pub);
+    showKey(prv);
 }
 
 int main()
@@ -21,30 +28,30 @@ int main()
         // Set version
         HDKeychain::setVersions(0x0488ADE4, 0x0488B21E);
 
+        cout << "Master (hex): " << SEED.getHex() << endl;
+
         // Set seed
-        HDSeed hdSeed(uchar_vector("000102030405060708090a0b0c0d0e0f"));
+        HDSeed hdSeed(SEED);
         bytes_t k = hdSeed.getMasterKey();
         bytes_t c = hdSeed.getMasterChainCode();
 
-        HDKeychain priv(0, 0, 0, c, k);
-        HDKeychain pub = priv.getPublic();
-        cout << "* [Chain m]" << endl;
-        show(pub);
-        show(priv);
+        stringstream chainname;
+        chainname << "Chain m";
+        HDKeychain prv(0, 0, 0, c, k);
+        HDKeychain pub = prv.getPublic();
+        showStep(chainname.str(), pub, prv);
 
-        priv = priv.getChild(0x80000000);
-        pub = priv.getPublic();
-        cout << "* [Chain m/0']" << endl;
-        show(pub);
-        show(priv);
+        //for (unsigned int k = 0; k < CHAIN_LENGTH; k++) {
+        chainname << "/0'";
+            prv = prv.getChild(0x80000000);
+            pub = prv.getPublic();
+            showStep(chainname.str(), pub, prv);
 
+        chainname << "/1";
         HDKeychain pub_ = pub.getChild(0x00000001);
-        priv = priv.getChild(0x00000001);
-        pub = priv.getPublic();
-        cout << "* [Chain m/0'/1]" << endl;
-        show(pub);
-        show(pub_);
-        show(priv);
+        prv = prv.getChild(0x00000001);
+        pub = prv.getPublic();
+        showStep(chainname.str(), pub, prv);
         return 0;
     }
     catch (const std::exception& e) {
